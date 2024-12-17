ac2753e074852411d71861d1223b551ee4a524ce
Marwa Yousef <marwa.yousef@gizasystems.com>
2024-12-17 15:28:36 +0200

    Search page & its' usage

--- tests/AdminPortal/TestLogin.spec.js
+++ tests/AdminPortal/TestLogin.spec.js
@@ -1,16 +1,21 @@
 const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../../src/Pages/AdminPortal/LoginPage');
 const { HomePage } = require('../../src/Pages/AdminPortal/HomePage');
+const { StreamManagementPage } = require('../../src/Pages/AdminPortal/Programs/StreamManagementPage');
 let loginPage;
 let homePage;
+let streamManagementPage;
 test('Login with valid credential', async ({ page }) => {
     loginPage = new LoginPage(page);
     homePage= new HomePage(page);
+    streamManagementPage= new StreamManagementPage(page);
     var baseUrl = global.testConfig.BASE_URL;
     var adminusername = global.testConfig.ADMIN_USER;
     var adminpassword = global.testConfig.ADMIN_PASS;
     await loginPage.gotoAdminPortal(baseUrl);
     var loginSuccess = await loginPage.login(adminusername, adminpassword);
     expect(loginSuccess).toBe(true);
+    /*await homePage.navigateToStreamsManagement();
+    await streamManagementPage.createProgram("PRG_StrSt_218");*/
     await homePage.logout();
     });
