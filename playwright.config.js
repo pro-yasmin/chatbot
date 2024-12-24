8e720db4cc81b8e1d1bf753fec8f81aa431e4e0c
Marwa Yousef <marwa.yousef@gizasystems.com>
2024-12-24 08:58:08 +0200

    integration with allure

--- playwright.config.js
+++ playwright.config.js
@@ -39,7 +39,11 @@ module.exports = defineConfig({
   workers: 1,
   //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['html', { outputFolder: 'playwright-report' }],
+  ['allure-playwright', { outputFolder: 'allure-results' }],   // Allure reporter
+ ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -51,13 +55,12 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on',
+    screenshot: 'only-on-failure', // Capture screenshot always
     actionTimeout: 150000, // Timeout for Playwright actions (15 seconds)
     navigationTimeout: 600000, // Timeout for page navigation (60 seconds)
 
-  },
-
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
-  /* Configure projects for major browsers */
+  }, 
+   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
