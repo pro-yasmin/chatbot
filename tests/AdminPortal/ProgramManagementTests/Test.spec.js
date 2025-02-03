a248c4650d3cf21339050940ab8d642e35118ef0
VEGA Laptop <sameh.3.4.2011@gmail.com>
2025-02-03 12:06:11 +0200

    no changes

--- tests/AdminPortal/ProgramManagementTests/Test.spec.js
+++ tests/AdminPortal/ProgramManagementTests/Test.spec.js
@@ -16,14 +16,13 @@ const { BenefitsPage } = require("../../../src/Pages/AdminPortal/ProgramsManagem
 const { BenefitsManagmentPage } = require("../../../src/Pages/AdminPortal/ProgramsManagement/BenefitsManagement/BenefitsManagementPage");
 const { BenefitsData } = require("../../../src/Models/AdminPortal/BenefitsData");
 const { Programs } = require("../../../src/Apis/Business/Programs");
-const { TasksPage } = require("../../../src/Pages/AdminPortal/Tasks/TasksPage.js");
 
 let loginPage,homePage,adminusername ,adminpassword , programs;
 let streamManagementPage,streamData,streamDetailsPage;
 let mainProgramPage, mainProgramManagementPage, mainProgramData ,mainProgramDetailsPage;
 let subProgramsManagementPage, subProgramsData, subProgramsPage,subProgramDetailsPage;
 let benefitsPage, benefitsManagmentPage, benefitsData;
-let streamNumber, tasksPage, subProgramNumber, benefitNumber ;
+let streamNumber, mainProgramNumber, subProgramNumber, benefitNumber ;
 let mainProgramTask , subProgramTask,benefitTask;
 
 
@@ -35,20 +34,19 @@ test.beforeEach(async ({ page }) => {
   loginPage = new LoginPage(page);
   homePage = new HomePage(page);
   streamManagementPage = new StreamManagementPage(page);
-  streamData = new StreamData(page);
+  streamData = new StreamData();
   streamDetailsPage = new StreamDetailsPage(page);
   mainProgramPage = new MainProgramPage(page);
   mainProgramManagementPage = new MainProgramManagementPage(page);
-  mainProgramData = new MainProgramData(page);
+  mainProgramData = new MainProgramData();
   mainProgramDetailsPage= new  MainProgramDetailsPage(page);
   subProgramsManagementPage = new SubProgramsManagementPage(page);
   subProgramsPage = new SubProgramsPage(page);
-  subProgramsData = new SubProgramsData(page);
+  subProgramsData = new SubProgramsData();
   subProgramDetailsPage= new SubProgramDetailsPage(page);
   benefitsPage = new BenefitsPage(page);
   benefitsManagmentPage = new BenefitsManagmentPage(page);
-  benefitsData = new BenefitsData(page);
-  tasksPage = new TasksPage(page);
+  benefitsData = new BenefitsData();
   programs = new Programs();
 
   var baseUrl = global.testConfig.BASE_URL;
@@ -67,7 +65,7 @@ test.beforeEach(async ({ page }) => {
 /**
  * Test case: Add Inside a new main program.
 */
-test.only("check", async () => {
+test.only("Add Inside New Main Program", async () => {
       // Step1: Create And Appove Stream From API
       await test.step("Create And Appove Stream From API", async () => {
       streamNumber = await programs.createStreamAndApproveAPI(adminusername, adminpassword, streamData) 
@@ -75,15 +73,42 @@ test.only("check", async () => {
       console.log('API Stream', streamNumber);
       });
 
-   // Step4: Approve new Program
-        await test.step("Approve Program Task", async () => {
-          console.log("Navigate to MyTasks page to approve Program Request");
-          await homePage.navigateToTasks();
-          var confirmMsg = global.testConfig.tasks.confirmStreamTask;
-          let True = await tasksPage.checkTaskRequestType(streamNumber[1] , confirmMsg);
-          expect(True).toBe(true);
-        });
+      // Step2: Navigate to Stream list page
+      await test.step("Navigate to StreamManagement page", async () => {
+      await homePage.navigateToStreamsManagement();
+      console.log("Click on Define New Main Program from Stream Details Page.");
+      await streamManagementPage.openViewStreamDetailsPage(streamNumber[1]);
+      await streamDetailsPage.InsideCreateMainProgram();
+
+      });
+
+      // Step3: Create Inside New Main Program from Stream Details Page.
+      await test.step("Create Inside New Program Task", async () => {
+      console.log("Main Program Creation Page Opened Successfully.");
+      var mainProgramresult = await mainProgramPage.createNewMainProgram(mainProgramData);
+      expect(mainProgramresult).toBe(true);
+      console.log("New Inside Main Program Created Successfully");
+      });
+
+    // Step4: Search on new Main Program
+      await test.step("Search on Main program created", async () => {
+        console.log("Search on Main Program created");
+        expect(await mainProgramManagementPage.checkMainProgramRowDetails( mainProgramData) ).toBe(true);
+        mainProgramNumber = mainProgramData.getCreatedMainProgramId();
+        console.log("New Main Program Details Checked Successfully");
+      });
+
+      // Step5: Approve the created Main Program.
+      await test.step("Approve Created Main Program Task", async () => {
+      console.log("Approve Created Main Program");
+      mainProgramTask = await programs.approveTaskAPI(adminusername, adminpassword,mainProgramNumber); 
+      expect(mainProgramTask).not.toBeNull(); 
+      console.log("New Inside Main Program Created Successfully");
+      });
+
 });
+
+
 
 /**
  * Test teardown: Logs out of the admin portal after each test.
