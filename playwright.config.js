b5cd0cd492ca4aced9d21da1d86073657b0b1bc8
Marwa Yousef <marwa.yousef@gizasystems.com>
2025-02-04 16:46:27 +0200

    change in playwright

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 1,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
   //workers: process.env.CI ? 1 : undefined,
@@ -125,7 +125,7 @@ module.exports = defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
  //
- ],
+],
 
 });
 module.exports.ENV = ENV;
