6fe95cfcd809efddc01e33d86bdf36d43f18b891
abdrahman.w.ibrahim <abdrahman.w.ibrahim@2024>
2025-02-17 16:46:26 +0200

    operation portal login TCs and change loginPage location

--- tests/AdminPortal/LookupsTests/TestViewLookUps.spec.js
+++ tests/AdminPortal/LookupsTests/TestViewLookUps.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@playwright/test');
-const { LoginPage } = require('../../../src/Pages/AdminPortal/LoginPage');
+const { LoginPage } = require('../../../src/Pages/LoginPage');
 const { HomePage } = require('../../../src/Pages/AdminPortal/HomePage');
 const { LookupsManagmentPage } = require('../../../src/Pages/AdminPortal/Lookups/LookupsManagmentPage');
 const { LookupPage } = require('../../../src/Pages/AdminPortal/Lookups/LookupPage');
