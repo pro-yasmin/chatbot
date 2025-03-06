deb40660393de8c1c30b9ad2be2e3d18661b0406
Marwa Yousef <marwa.yousef@gizasystems.com>
2025-03-06 14:25:38 +0200

    maximize screen

--- playwright.config.js
+++ playwright.config.js
@@ -35,6 +35,9 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     browserName: 'chromium',
+    launchOptions: {
+      args: ['--start-maximized']
+    },
     viewport: null,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
